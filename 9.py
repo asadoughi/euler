5851b9b856225eea50c5d8af1bde5e361bed85ee
Amir Sadoughi <amir.sadoughi@gmail.com>
2012-12-01 20:15:58 -0600

    9.py

---
+++ 9.py
@@ -0,0 +1,7 @@
+
+for a in range(1,1001):
+    for b in range(1,1001):
+        for c in range(1,1001):
+            if a + b + c == 1000 and a*a + b*b == c*c:
+                print a*b*c
+                exit()
