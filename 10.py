6f817a555a7b2aba98de654229b9bfb5f308da26
Amir Sadoughi <amir.sadoughi@gmail.com>
2012-12-01 20:17:22 -0600

    10.py

---
+++ 10.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def sieve_of_eratosthenes(max_value):
+    primes = range(2, max_value + 1)
+    i = 0
+    while i < len(primes):
+        for j in range(i + primes[i], len(primes), primes[i]):
+            primes[j] = 0
+        i += 1
+        while i < len(primes) and primes[i] == 0:
+            i += 1
+    primes = filter(lambda x: x != 0, primes)
+    return primes
+    
+primes = sieve_of_eratosthenes(2000000)
+print sum(primes)
